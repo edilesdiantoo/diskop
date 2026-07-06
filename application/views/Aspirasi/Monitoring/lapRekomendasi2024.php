dd2b03735a3928d947decdd4f12f883d56f8a1a7
edilesdiantoo <edilesdianto@gmail.com>
2026-07-06 10:05:30 +0700

    penambahan tahun 2026

--- application/views/Aspirasi/Monitoring/lapRekomendasi2024.php
+++ application/views/Aspirasi/Monitoring/lapRekomendasi2024.php
@@ -11,6 +11,7 @@
                 <option value="2023">2023</option>
                 <option value="2024">2024</option>
                 <option value="2025">2025</option>
+                <option value="2026">2026</option>
             </select>
             <select class="form-control mr-3" id="kab" name="kab" required>
                 <option value="">Pilih KAB. </option>
--- index.php
+++ index.php
@@ -65,27 +65,27 @@ define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'developm
  * By default development will show errors but testing and live will hide them.
  */
 switch (ENVIRONMENT) {
-	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
-		break;
+    case 'development':
+        // Mengaktifkan semua error, KECUALI E_DEPRECATED dan E_USER_DEPRECATED
+        error_reporting(E_ALL & ~E_DEPRECATED & ~E_USER_DEPRECATED);
+        ini_set('display_errors', 1);
+        break;
 
-	case 'testing':
-	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>=')) {
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		} else {
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
-		break;
+    case 'testing':
+    case 'production':
+        ini_set('display_errors', 0);
+        if (version_compare(PHP_VERSION, '5.3', '>=')) {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+        } else {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+        }
+        break;
 
-	default:
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
-		exit(1); // EXIT_ERROR
+    default:
+        header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+        echo 'The application environment is not set correctly.';
+        exit(1); // EXIT_ERROR
 }
-
 /*
  *---------------------------------------------------------------
  * SYSTEM DIRECTORY NAME
